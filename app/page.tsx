9e14391be0158013c42dbcb030a5fa15b29d3450
mizzzuno <mizz22@outlook.jp>
2025-10-26 01:52:43 +0900

    fix: format to prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 // import MonthSelect from "@/app/ui/staff/MonthSelect";
-import dynamic from 'next/dynamic'
-const NoSSR = dynamic(() => import("@/app/ui/staff/MonthSelect"), { ssr: false })
+import dynamic from "next/dynamic";
+const NoSSR = dynamic(() => import("@/app/ui/staff/MonthSelect"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
