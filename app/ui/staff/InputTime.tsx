162c74f377d0296a9adab6dfbd049635f4c59c8b
mizzzuno <mizz22@outlook.jp>
2025-10-29 23:41:29 +0900

    fix: change datepicker to datetimepicker

--- app/ui/staff/InputTime.tsx
+++ app/ui/staff/InputTime.tsx
@@ -1,19 +1,22 @@
-import dayjs from "dayjs";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { MobileTimePicker } from "@mui/x-date-pickers/MobileTimePicker";
+import dayjs from 'dayjs';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { MobileDateTimePicker } from '@mui/x-date-pickers/MobileDateTimePicker';
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
-export default function ResponsiveTimePickers() {
+export default function ResponsiveDateTimePickers() {
+
   const now = dayjs().utc();
   const jst = now.tz("Asia/Tokyo");
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <MobileTimePicker defaultValue={dayjs(jst.format("YYYY-MM-DDTHH:mm"))} />
+          <MobileDateTimePicker defaultValue={dayjs('2022-04-17T15:30')} />
     </LocalizationProvider>
   );
 }
+
